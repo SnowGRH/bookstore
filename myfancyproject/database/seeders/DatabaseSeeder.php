3c6b2aca4ee8e41b790f6c2560e5e537b04ab5e4
SnowGRH <gergelyovinczeff@gmail.com>
2023-09-12 11:06:15 +0200

    random teszt adatok fel töltése

--- myfancyproject/database/seeders/DatabaseSeeder.php
+++ myfancyproject/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\book::factory(10)->create();
+        \App\Models\copies::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
